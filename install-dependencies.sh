a0d547081d8b9f683ac8116fcf54ebcd0cc31600
Olga Vechtomova <104406517+instructormsci@users.noreply.github.com>
2024-02-04 23:20:18 -0500

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-# Load NVM
+# Install NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# The script clones the nvm repository to ~/.nvm 
+# and adds the source line to your profile (‘~/.bashrc’, ‘~/.zshrc’, ‘~/.profile’, or ‘~/.bash_profile’)
+
+# Load NVM 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
-# Install and use Node version specified in .nvmrc
-nvm install
-nvm use
+# Install Node.js version 16
+nvm install 16
+nvm use 16
 
-# Run yarn to install JavaScript dependencies
+# Install dependencies with yarn
 yarn
