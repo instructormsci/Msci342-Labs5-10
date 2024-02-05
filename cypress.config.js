56a4589fc93097056e1fe07822f2be87f15bf97c
Olga Vechtomova <104406517+instructormsci@users.noreply.github.com>
2024-02-05 01:31:15 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require('cypress');
+const {defineConfig} = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.CODESPACES ? `https://${process.env.CODESPACE_NAME}-3000.githubpreview.dev` : 'http://localhost:3000',
+    baseUrl: 'http://localhost:3000',
   },
 });
