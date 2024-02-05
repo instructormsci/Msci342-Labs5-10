27de9536d0bf3530ff6f80711e3f19aa47392189
Olga Vechtomova <104406517+instructormsci@users.noreply.github.com>
2024-02-04 23:09:15 -0500

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-nvm install 16.17.1
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Install and use the specific Node.js version
+nvm install 16
 nvm use 16
 
+# Run yarn to install JavaScript dependencies
 yarn
+
