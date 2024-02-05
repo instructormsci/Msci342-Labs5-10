8bf97069d23fde5f5852348aa3aceb848f9210fa
Olga Vechtomova <104406517+instructormsci@users.noreply.github.com>
2024-02-04 23:45:36 -0500

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -3,11 +3,12 @@
 # Manually initialize nvm based on your .bashrc configuration
 export NVM_DIR="/usr/local/share/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Now use NVM commands
+# Install Node.js version 16 and set it as the default
 nvm install 16
-nvm use 16
+nvm alias default 16
+nvm use default
 
 # Install dependencies with yarn
 yarn
+
